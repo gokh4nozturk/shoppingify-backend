3f02360e9eb79e35e083f3dc74e2476720b8754f
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-09 11:28:39 +0300

    improvements

--- src/history.ts
+++ src/history.ts
@@ -6,6 +6,7 @@ export const historySchema = new mongoose.Schema({
   createdAt: { type: Date, required: false, default: Date.now },
   visible: { type: Boolean, required: false, default: true },
   completed: { type: Boolean, required: false, default: false },
+  pieces: { type: Number, required: true, default: 0 },
 });
 
 const History = mongoose.model("History", historySchema);

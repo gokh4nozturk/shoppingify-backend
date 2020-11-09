c526f42289d0c804f69155822ff36314776e031d
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-09 10:17:34 +0300

    improvemetns

--- src/history.ts
+++ src/history.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const historySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  listItem: { type: Object, required: true },
+  listItem: { type: Array, required: true },
   createdAt: { type: Date, required: false, default: Date.now },
   visible: { type: Boolean, required: false, default: true },
+  completed: { type: Boolean, required: false, default: false },
 });
 
 const History = mongoose.model("History", historySchema);

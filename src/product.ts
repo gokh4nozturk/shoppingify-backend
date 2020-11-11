2627070f1d7abecaf2a14acb2609428cf6964787
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-11 15:45:21 +0300

    product updated

--- src/product.ts
+++ src/product.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
-  note: { type: String, required: true },
+  note: { type: String, required: false },
   image: { type: String, required: false },
   createdAt: { type: Date, required: false, default: Date.now },
   visible: { type: Boolean, required: false, default: true },
